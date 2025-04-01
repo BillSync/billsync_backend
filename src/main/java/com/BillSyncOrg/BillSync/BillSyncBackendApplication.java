ea61988b180c1f6a1dc8cdaffbbd8431645fc0d3
Khushi Shah <67200869+khushi2699@users.noreply.github.com>
2025-04-01 15:31:39 -0400

    Adding untracked files

---
+++ src/main/java/com/BillSyncOrg/BillSync/BillSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BillSyncOrg.BillSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSyncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BillSyncOrg/BillSync/BillSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BillSyncOrg.BillSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
